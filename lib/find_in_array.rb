0aaf0ddef57309c09a67f15896b81289f35e83b7
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-10 23:01:22 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
   count = 0
-  found_value_index = nil
   while count < array.length do
     if array[count] == value_to_find
       return count
   end
   count += 1
+end
 end
